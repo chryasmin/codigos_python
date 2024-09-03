6ffaa36d62e1b5f2fa42fde18202857078d03b62
Lisbela <lisbelagb@gmail.com>
2024-09-03 11:52:00 -0300

    Adicionando exercicios

---
+++ atividades/atividade2.py
@@ -0,0 +1,7 @@
+comprimento = float(input("Informe seu comprimento: "))
+largura = float (input("Informe sua largura: "))
+altura = float (input("Informe sua altura: "))
+
+volume = comprimento*largura*altura
+
+print(f"O resultado do volume é: {volume:.2f}")
